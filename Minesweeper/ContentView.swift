696cd0c42a1ef279352a5369bbe5e0328f3ed421
evgenishko <plastinin.evgeny@gmail.com>
2026-01-12 10:25:19 +0300

    Initial Commit

---
+++ Minesweeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Minesweeper
+//
+//  Created by Evgeny Plastinin on 12.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Evgeny Plastinin on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
